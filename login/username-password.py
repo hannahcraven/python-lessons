b56b53906f1c60b399e08f5a087d73891842ea8e
Hannah Craven <hannah@cravencode.com>
2016-10-29 12:40:15 -0400

    added username password script

---
+++ login/username-password.py
@@ -0,0 +1,13 @@
+name = input("what is your Username: ")
+password = input("What is your Password: ")
+print("To lock your computer type lock.")
+command = None
+input1 = None
+input2 = None
+while command != "lock":
+    command = input("What is your command: ")
+while input1 != name:
+    input1 = input("What is your username: ")
+while input2 != password:
+    input2 = input("What is your password:")
+print("welcome back to the system!")
